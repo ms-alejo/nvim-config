a579db87c57b15efb3c91bd3df368fad9ed72fe4
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2024-12-13 02:44:46 -0700

    added move line mappings

--- init.lua
+++ init.lua
@@ -42,6 +42,18 @@ vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+-- move visually select lines up or down
+vim.keymap.set("n", "K", ":m .-2<CR>==", { noremap = true })
+vim.keymap.set("v", "K", ":m <-2<CR>gv=gv", { noremap = true })
+vim.keymap.set("n", "J", ":m .+1<CR>==", { noremap = true })
+vim.keymap.set("v", "J", ":m >+1<CR>gv=gv", { noremap = true })
+
+-- window navigation keymaps
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
 -- AutoCommands
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
