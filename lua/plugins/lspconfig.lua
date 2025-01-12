184ac2ea1eceb325cd5c5f3c1ea84cadff17a10d
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2025-01-12 12:08:45 -0700

    update go related things and update after 3 weeks no update

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -180,10 +180,37 @@ return {
         gopls = {
           settings = {
             gopls = {
-              completeUnimported = true,
-              analyses = {
-                unusedparams = true,
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
               },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
             },
           },
         },
@@ -233,7 +260,6 @@ return {
         "black",
         "gofumpt",
         "goimports-reviser",
-        "golines",
       })
       require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
