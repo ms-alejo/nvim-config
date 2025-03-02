5257366442ff46fe44530b6b0f4611a5bccc78b0
Miguel Alejo <miguel.sebastian.alejo@gmail.com>
2025-03-02 00:21:43 -0700

    added ghost text and updated sorting in cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -40,6 +40,7 @@ return {
       -- See `:help cmp`
       local cmp = require "cmp"
       local luasnip = require "luasnip"
+      local compare = require "cmp.config.compare"
       luasnip.config.setup {}
 
       cmp.setup {
@@ -55,6 +56,38 @@ return {
           documentation = cmp.config.window.bordered(),
         },
 
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            compare.offset,
+            compare.exact,
+            compare.score,
+
+            -- cmp_under_comparator
+            function(entry1, entry2)
+              local _, entry1_under = entry1.completion_item.label:find "^_+"
+              local _, entry2_under = entry2.completion_item.label:find "^_+"
+              entry1_under = entry1_under or 0
+              entry2_under = entry2_under or 0
+              if entry1_under > entry2_under then
+                return false
+              elseif entry1_under < entry2_under then
+                return true
+              end
+            end,
+
+            compare.recently_used,
+            compare.locality,
+            compare.kind,
+            compare.sort_text,
+            compare.length,
+            compare.order,
+          },
+        },
+
+        experimental = {
+          ghost_text = true,
+        },
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
         --
